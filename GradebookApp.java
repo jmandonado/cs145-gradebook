4d1482c0cbdaac0e443abb3544d04e7b63811910
jmandonado <jmandonado@live.nmhu.edu>
2016-05-13 15:10:47 -0600

    'gradebook'

--- GradebookApp.java
+++ GradebookApp.java
@@ -1,9 +1,14 @@
-// GradebookApp.java v.20160502
+// Joseph Mandonado Gradebook
 
-class GradebookApp {
-	
-	public static void main(String[] args) {
-
-	}
-
-}
+ class GradebookApp {
+ 	
+ 	public static void main(String[] args) {
+		String name_1 = "Sam";
+		Student student_1 =  new Student(name_1);
+		System.out.println("High: " + student_1.high());
+		System.out.println("Low: " + student_1.low());
+		System.out.println("Average: " + student_1.average());
+		System.out.println("Letter Grade: " + Student.letter_grade(student_1.average()));
+ 	}
+ 
+ }
--- Student.java
+++ Student.java
@@ -1,5 +1,60 @@
-// Student.java v.20160502
-
+// Joseph Mandonado Gradebook
+import java.util.Random;
 class Student {
+	String name;
+	double[] grade = new double[10];
 
+	public Student(String n) {
+		name = n;
+		for (int i = 0; i < 10; i++){
+			grade[i] = (Math.random() * 100) + 1;
+			System.out.println(grade[i]);
+		}
+	}
+
+	public double high() {
+		double max = grade[0];
+
+        for(int i = 0; i < 10; i++) {
+            if (grade[i] > max) {
+                max = grade[i];
+            }
+        }
+        return max;
+	}
+
+	public double low() {
+		double min = grade[0];
+
+        for(int i = 0; i < 10; i++) {
+            if (grade[i] < min) {
+                min = grade[i];
+            }
+        }
+		return min;
+	}
+
+	public double average() {
+		double sum = 0;        
+        for(int i = 0; i < 10; i++) {
+            sum = sum + grade[i];
+        }
+        double average = (sum / 10);
+		return average;
+	}
+
+	static char letter_grade(double average) {
+		char let_grade = ' ';
+		if (average >= 90 && average <= 100)
+			let_grade = 'A';
+		if (average >= 80 && average <= 89.99)
+			let_grade = 'B';
+		if (average >= 70 && average <= 79.99)
+			let_grade = 'C';
+		if (average >= 60 && average <= 69.99)
+			let_grade = 'D';
+		else
+			let_grade = 'F';
+		return let_grade;
+	}
 }
